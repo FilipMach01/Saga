1e9c047db04f63f56e7f104b284436a3bd804c56
filip <filip.mach01@gmail.com>
2024-01-31 14:48:51 +0100

    background movement

--- Saga.py
+++ Saga.py
@@ -7,13 +7,14 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Saga!')
 
 speed = 4
-
+background_speed = 3
 # clock
 clock = pygame.time.Clock()
 fps = 60
 # background
 background_surf = pygame.image.load("graphic/background.png").convert()
-background_blit = background_surf,(0,0)
+background_surf = pygame.transform.scale(background_surf, (1600,1200))
+background_rect = background_surf.get_rect(center = (400,300))
 
 # player
 player_surf_front = pygame.image.load("graphic/player_from_front.png").convert_alpha()
@@ -21,9 +22,9 @@ player_rect = player_surf_front.get_rect(center = (400,300))
 
 player_surf_back = pygame.image.load("graphic/player_from_back.png").convert_alpha()
 
-player_surf_left = pygame.image.load("graphic/player_from_left.jpg").convert_alpha()
+player_surf_left = pygame.image.load("graphic/player_from_left.png").convert_alpha()
 
-player_surf_right = pygame.image.load("graphic/player_from_right.jpg").convert_alpha()
+player_surf_right = pygame.image.load("graphic/player_from_right.png").convert_alpha()
 
 Run = True
 while Run:
@@ -31,23 +32,37 @@ while Run:
   for event in pygame.event.get():
       if event.type == QUIT:
           Run = False
-  screen.blit(background_surf,(0,0))          
+            
   keys = pygame.key.get_pressed()
+  screen.blit(background_surf,background_rect)
   if keys[pygame.K_w]:
-    player_rect.top -= speed
+    background_rect.top += background_speed
+    if background_rect.top > 0:
+      background_rect.top = 0
     screen.blit(player_surf_back, player_rect)
+  
   elif keys[pygame.K_s]:
-    player_rect.bottom += speed
-    screen.blit(player_surf_front, player_rect)
-  elif keys[pygame.K_a]:
-    player_rect.left -= speed
-    screen.blit(player_surf_left, player_rect)
-  elif keys[pygame.K_d]:
-    player_rect.right += speed
-    screen.blit(player_surf_right, player_rect)
-  else:
+    background_rect.top -= background_speed
+    if background_rect.top < -600: 
+      background_rect.top = -600
     screen.blit(player_surf_front, player_rect)
   
-
+  elif keys[pygame.K_a]:
+    background_rect.left += background_speed
+    if background_rect.left > 0:
+        background_rect.left = 0
+    screen.blit(player_surf_left, player_rect)
+  
+  elif keys[pygame.K_d]:
+    background_rect.right -= background_speed
+    if background_rect.right < 800:
+        background_rect.right = 800
+    screen.blit(player_surf_right, player_rect)
+  
+  else: 
+     screen.blit(player_surf_front, player_rect) 
+  
+  
+    
   
   pygame.display.update()
